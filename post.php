aa629466768e728c94cf4e545b3a5ad6528da486
Andrea <ajbb961111@gmail.com>
2020-05-04 05:02:46 -0500

    proyect pillow

---
+++ post.php
@@ -0,0 +1,24 @@
+<?php
+
+$nameP = $_POST['nameP'];
+$nameM = $_POST['nameM'];
+$son = $_POST['son'];
+$age = $_POST['age'];
+$phone = $_POST['phone'];
+$phone = $_POST['email'];
+$message = $_POST['message'];
+
+// var $parents = $nameP . ", " . $nameM
+
+// Datos para el correo
+$to = "ajbb961111@gmail.com";
+$asunto = "TEST";
+
+$carta = "De: $nameP \n";
+$carta .= "Hijos: $son \n";
+$carta .= "Edad: $age \n";
+$carta .= "Telefono: $phone \n";
+$carta .= "Mensaje: $message \n";
+
+    // Enviando Mensaje
+    mail($to, $asunto, $carta);
